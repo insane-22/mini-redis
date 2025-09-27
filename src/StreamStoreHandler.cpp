57f955b85c1761888792e12da3d4e55f71846313
insane-22 <bumikabachchan11315@gmail.com>
2025-09-27 18:18:32 +0530

    trial

--- src/StreamStoreHandler.cpp
+++ src/StreamStoreHandler.cpp
@@ -194,7 +194,7 @@ void StreamStoreHandler::handleXread(const std::vector<std::string>& tokens) {
         }
 
         if(results.empty()) {
-            response = "*-1\r\n"; 
+            response += "*-1\r\n"; 
             continue;
         }
 
